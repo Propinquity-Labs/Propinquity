0db5434469aef7939e1942ab1ffdebd681514ea4
CaelumD25 <caelumdudek25@gmail.com>
2025-06-03 00:44:18 -0700

    feat(nav): Base nav working

---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,12 @@
+import "package:flutter/widgets.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: implement build
+    return const Center(child: Text("Home"));
+  }
+}
---
+++ lib/presentation/screens/modify_screen.dart
@@ -0,0 +1,12 @@
+import "package:flutter/widgets.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
+class ModifyScreen extends ConsumerWidget {
+  const ModifyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: implement build
+    return const Center(child: Text("Modify/add"));
+  }
+}
